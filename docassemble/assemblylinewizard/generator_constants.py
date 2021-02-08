0ef04738fd3fa9b85c52f37ffd43a0c4c82a7929
Anonymous User <52798256+plocket@users.noreply.github.com>
2021-02-08 10:04:34 -0500

    Close #181 add mobile_number. For consistency, mobile and phone_number

--- docassemble/assemblylinewizard/generator_constants.py
+++ docassemble/assemblylinewizard/generator_constants.py
@@ -119,6 +119,9 @@ generator_constants.PEOPLE_SUFFIXES_MAP = {
   '_age': ".age_in_years()",
   '_email': ".email",
   '_phone': ".phone_number",
+  '_phone_number': ".phone_number",
+  '_mobile': ".mobile_number",
+  '_mobile_number': ".mobile_number",
   '_phones': ".phone_numbers()",
   '_address_block': ".address.block()",
   # TODO: deprecate street and street2 from existing forms and documentation
