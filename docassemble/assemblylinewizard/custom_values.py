9d74c6b97afbfddeb8d3278fb0816224568ad038
Anonymous User <52798256+plocket@users.noreply.github.com>
2021-03-04 16:30:56 -0500

    Fix comment from object to dict

--- docassemble/assemblylinewizard/custom_values.py
+++ docassemble/assemblylinewizard/custom_values.py
@@ -9,6 +9,6 @@ class Object(object):
 custom_values = Object()
 
 # Var names of people-type lists that developers add while using the weaver.
-# Could be a list, but an object will match RESERVED_PLURALIZERS_MAP in the
+# Could be a list, but a dict will match RESERVED_PLURALIZERS_MAP in the
 # places it's used, so maybe easier to treat them both the same.
 custom_values.people_plurals_map = {}
