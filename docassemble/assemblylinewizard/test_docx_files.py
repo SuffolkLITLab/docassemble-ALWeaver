06eeaac70a510b9328f538936d092384df3a22a5
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-05-22 06:07:59 -0400

    Add test for a few reserved DOCX labels

--- docassemble/assemblylinewizard/test_docx_files.py
+++ docassemble/assemblylinewizard/test_docx_files.py
@@ -1,5 +1,5 @@
 import unittest
-from .interview_generator import DAField, get_docx_variables
+from .interview_generator import DAField, get_docx_variables, is_reserved_docx_label
 from docx2python import docx2python
 from pathlib import Path
 
@@ -21,3 +21,17 @@ class test_docxs(unittest.TestCase):
     self.assertIn('children[0].address.address', all_vars)
     self.assertIn('children[0].address.county', all_vars)
     self.assertIn('milkman.attorney.firm', all_vars)
+
+  def test_reserved_docx_labels(self):
+    reserved_labels_files = Path(__file__).parent / 'test/reserved_docx_variables.docx'
+    docx_data = docx2python(unmap_suffixes_file)
+    text = docx_data.text
+    all_vars = get_docx_variables(text)
+    reserved_labels = []
+    for label in all_vars:
+      if is_reserved_docx_label(label):
+        reserved_labels.append(label)
+    self.assertEqual(len(reserved_labels), 2, str(reserved_labels))
+    self.assertIn('trial_court.address.county', reserved_labels)
+    self.assertIn('users[0].address.address', reserved_labels)
+    self.assertNotIn('users.address.zip', reserved_labels)
