939341b3d74cc493038e31bf6f58981e6ed0cbc2
Bryce Willey <BryceStevenWilley@users.noreply.github.com>
2021-05-25 00:13:55 -0400

    Yaml instead of JSON, and split into jurisdiction and org questions

--- docassemble/assemblylinewizard/custom_values.py
+++ docassemble/assemblylinewizard/custom_values.py
@@ -1,15 +1,15 @@
 from typing import List, Union
 from pathlib import Path
-import json
-from docassemble.base.util import DADict
+import yaml
+from docassemble.base.util import log, DADict
+from docassemble.base.core import objects_from_file
 
 """
 Container for widely used data that may be altered by user inputs.
 """
 
-__all__ = ['get_possible_dependencies', 'get_pypi_deps_from_choices', 'get_yml_deps_from_choices', 
-           'get_is_default_from_choices']
-
+__all__ = ['get_possible_deps_as_choices', 'get_pypi_deps_from_choices', 
+           'get_yml_deps_from_choices']
 
 # This is to workaround fact you can't do local import in Docassemble playground
 class Object(object):
@@ -22,53 +22,56 @@ custom_values = Object()
 # places it's used, so maybe easier to treat them both the same.
 custom_values.people_plurals_map = {}
 
-
 # Filled in by load_org_specific. Is a dictionary of configs that each org using
 # the Weaver can set, per installation. The specific configs are:
-# * dependency choices: a collection of other yaml files that the generated interview will include
+# * jurisdiction_dependency_choices: a collection of other yaml files 
+#   that the generated interview will include, specifically a jurisdiction
 #   It's a map from keys, what the user will see when selecting dependencies, that maps to
 #   a list of 3 items: the pypi dependency, the yaml include, and a boolean for default selection
 custom_values.org_specific_config = None
 
 def load_org_specific(all_custom_values=custom_values):
   if all_custom_values.org_specific_config is None:
-    current_dir = Path(__file__).resolve().parent
-    config_file = current_dir.joinpath('data/sources/org_specific.cfg')
-    if config_file.exists():
-      all_custom_values.org_specific_config = json.load(config_file.open())
-    else:
+    try:
+      all_custom_values.org_specific_config = objects_from_file('org_specific.yml')
+    except SystemError as ex:
       # Populate some default values
       all_custom_values.org_specific_config = {
         'dependency_choices': {
           'Massachusetts State': 
               ['docassemble.ALMassachusetts>=0.0.7', 
-               'docassemble.ALMassachusetts:al_massachusetts.yml', 
+               'docassemble.ALMassachusetts:al_massachusetts.yml',
+               'jurisdiction',
                True],
           'MassAccess': 
               ['docassemble.MassAccess',  
-               'docassemble.MassAccess:massaccess.yml', 
+               'docassemble.MassAccess:massaccess.yml',
+               'organization',
                True]
         }
       }
 
-def get_possible_dependencies(all_custom_values=custom_values):
+def get_possible_deps_as_choices(dep_category=None, all_custom=custom_values):
   """Gets the possible yml files that the generated interview will depend on"""
-  load_org_specific(all_custom_values)
-  return all_custom_values.org_specific_config['dependency_choices'].keys()
+  load_org_specific(all_custom)
+  dep_choices = all_custom.org_specific_config['dependency_choices']
+  if dep_category is None:
+    return [{dep_key: dep_key, 'default': dep[3]} for dep_key, dep in dep_choices.items()]
+  else:
+    return [{dep_key: dep_key, 'default': dep[3]} for dep_key, dep in dep_choices.items() 
+            if dep[2].lower() == dep_category.lower()]
 
-
-def get_pypi_deps_from_choices(choices:Union[List[str], DADict], 
-    all_custom_values=custom_values):
+def get_pypi_deps_from_choices(choices:Union[List[str], DADict],
+    all_custom=custom_values):
+  """Gets the Pypi dependency requirement (i.e. docassemble.AssemblyLine>=2.0.19)
+  from some chosen dependencies"""
   return get_values_from_choices(choices, 0)
 
-def get_yml_deps_from_choices(choices:Union[List[str], DADict], 
+def get_yml_deps_from_choices(choices:Union[List[str], DADict],
     all_custom_values=custom_values):
+  """Gets the yml file (i.e. docassemble.AssemblyLine:data/question/ql_baseline.yml)
+  from some chosen dependencies"""
   return get_values_from_choices(choices, 1)
-
-def get_is_default_from_choices(all_custom_values=custom_values):
-  load_org_specific(all_custom_values)
-  return [dependency[0] for dependency in 
-      all_custom_values.org_specific_config['dependency_choices'].items() if dependency[1][2]] 
 
 def get_values_from_choices(choices:Union[List[str], DADict], value_idx:int=0,
     all_custom_values=custom_values):
