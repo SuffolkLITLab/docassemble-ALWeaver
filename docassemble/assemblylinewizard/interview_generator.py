bbf388c06a04e60004fdddd5482963349f9515b8
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-05-28 17:04:57 -0400

    Fix merge conflicts

--- docassemble/assemblylinewizard/interview_generator.py
+++ docassemble/assemblylinewizard/interview_generator.py
@@ -489,12 +489,10 @@ class DAField(DAObject):
     else:
       return self.final_display_var
 
-  def get_settable_var(self, 
-      display_to_settable_suffix=generator_constants.DISPLAY_SUFFIX_TO_SETTABLE_SUFFIX):
+  def get_settable_var(self, display_to_settable_suffix=generator_constants.DISPLAY_SUFFIX_TO_SETTABLE_SUFFIX):
     return substitute_suffix(self.final_display_var, display_to_settable_suffix)
   
-  def _get_attributes(self, full_display_map=generator_constants.FULL_DISPLAY,
-      primitive_pluralizer=generator_constants.RESERVED_PRIMITIVE_PLURALIZERS_MAP):
+  def _get_attributes(self, full_display_map=generator_constants.FULL_DISPLAY):
     """Returns attributes of this DAField, notably without the leading "prefix", or object name
     * the plain attribute, not ParentCollection, but the direct attribute of the ParentCollection
     * the "full display", an expression that shows the whole attribute in human readable form
@@ -505,20 +503,14 @@ class DAField(DAObject):
     label_parts = re.findall(r'([^.]*)(\..*)*', self.get_settable_var())
 
     prefix_with_index = label_parts[0][0] 
-    prefix = re.sub(r'\[.+\]', '', prefix_with_index)
+
     settable_attribute = label_parts[0][1].lstrip('.')
-    if prefix in primitive_pluralizer.keys():
-      # It's just a primitive (either by itself or in a list). I.e. docket_numbers
-      settable_attribute = ''
-      plain_att = ''
-      full_display_att = ''
-    else:
-      if settable_attribute == '' or settable_attribute == 'name':
-        settable_attribute = 'name.first'
-      if settable_attribute == 'address' or settable_attribute == 'mailing_address':
-        settable_attribute += '.address'
-      plain_att = re.findall(r'([^.]*)(\..*)*', settable_attribute)[0][0]
-      full_display_att = substitute_suffix('.' + plain_att, full_display_map).lstrip('.')
+    if settable_attribute == '' or settable_attribute == 'name':
+      settable_attribute = 'name.first'
+    if settable_attribute == 'address' or settable_attribute == 'mailing_address':
+      settable_attribute += '.address'
+    plain_att = re.findall(r'([^.]*)(\..*)*', settable_attribute)[0][0]
+    full_display_att = substitute_suffix('.' + plain_att, full_display_map).lstrip('.')
     return (plain_att, full_display_att, settable_attribute)
 
 
@@ -573,8 +565,7 @@ class ParentCollection(object):
     if self.var_type != 'primitive':  # this base var is more complex than a simple primitive type
       for f in self.fields:
         plain_att, disp_att, settable_att = f._get_attributes()
-        if plain_att:
-          self.attribute_map[plain_att] = (disp_att, settable_att)
+        self.attribute_map[plain_att] = (disp_att, settable_att)
 
   def table_page(self) -> str:
     # TODO: migrate to Mako format
@@ -594,10 +585,6 @@ confirm: True
       all_columns += '  - {0}: |\n'.format(att)
       all_columns += '      row_item.{0} if defined("row_item.{1}") else ""\n'.format( disp_and_set[0], disp_and_set[1])
       settable_list += '  - {}\n'.format(disp_and_set[1])
-    if len(self.attribute_map) == 0:
-      all_columns += '  - name: |\n'
-      all_columns += '      row_item\n'
-      settable_list += '  True\n'
     return content.format(var_name=self.var_name, all_columns=all_columns.rstrip('\n'), settable_list=settable_list.rstrip('\n'))
 
   def review_yaml(self): 
@@ -999,8 +986,7 @@ def is_reserved_docx_label(label, docx_only_suffixes=generator_constants.DOCX_ON
                            reserved_whole_words=generator_constants.RESERVED_WHOLE_WORDS,
                            undefined_person_prefixes=generator_constants.UNDEFINED_PERSON_PREFIXES,
                            reserved_pluralizers_map=generator_constants.RESERVED_PLURALIZERS_MAP,
-                           reserved_suffixes_map=generator_constants.RESERVED_SUFFIXES_MAP,
-                           allow_singular_suffixes=generator_constants.ALLOW_SINGULAR_SUFFIXES):
+                           reserved_suffixes_map=generator_constants.RESERVED_SUFFIXES_MAP):
     '''Given a string, will return whether the string matches
     reserved variable names. `label` must be a string.'''
     if label in reserved_whole_words:
@@ -1026,9 +1012,7 @@ def is_reserved_docx_label(label, docx_only_suffixes=generator_constants.DOCX_ON
       docx_only_suffixes_regex = '^' + '$|^'.join(docx_only_suffixes) + '$'
       docx_suffixes_matches = re.findall(docx_only_suffixes_regex, suffix)
       if (suffix in reserved_suffixes_map.values()
-          or len(docx_suffixes_matches) > 0) and (prefix in allow_singular_suffixes or label_parts[0][0].endswith(']')):
-          # We do NOT want users.address.address to match, only users[0].address.address
-          # but we do allow trial_court.address.address. Make sure we don't overmatch
+          or len(docx_suffixes_matches) > 0):
         return True
 
     # For all other cases
