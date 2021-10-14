47344abfc5cab649f4ebfd7863212cf6371b74c2
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-10-14 14:10:53 -0400

    Subclass instead of copying for DebugUndefined class

--- docassemble/ALWeaver/validate_docx_files.py
+++ docassemble/ALWeaver/validate_docx_files.py
@@ -1,4 +1,4 @@
-from jinja2 import Undefined
+from jinja2 import Undefined, DebugUndefined
 from jinja2.utils import missing
 from docxtpl import DocxTemplate
 from jinja2 import Environment, BaseLoader
@@ -7,28 +7,10 @@ from docassemble.base.util import DAFile
 
 __all__ = ['CallAndDebugUndefined', 'get_jinja_errors']
 
-class CallAndDebugUndefined(Undefined):
+class CallAndDebugUndefined(DebugUndefined):
     """Handles Jinja2 undefined errors by printing the name of the undefined variable.
     Extended to handle callable methods.
-    """
-
-    __slots__ = ()
-
-    def __str__(self) -> str:
-        if self._undefined_hint:
-            message = f"undefined value printed: {self._undefined_hint}"
-
-        elif self._undefined_obj is missing:
-            message = self._undefined_name  # type: ignore
-
-        else:
-            message = (
-                f"no such element: {object_type_repr(self._undefined_obj)}"
-                f"[{self._undefined_name!r}]"
-            )
-
-        return f"{{{{ {message} }}}}"
-      
+    """      
     def __call__(self, *pargs, **kwargs):
         return self
       
