8b41ea96cad81e4962c41bbc7508ea4021fe2668
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-12-23 17:57:24 -0500

    Get the config to work through download screen

--- docassemble/ALWeaver/custom_values.py
+++ docassemble/ALWeaver/custom_values.py
@@ -1,4 +1,3 @@
-# pre-load
 from typing import List, Union
 from pathlib import Path
 import ruamel.yaml as yaml
@@ -11,6 +10,14 @@ import sys
 from docassemble.base.functions import package_data_filename
 from packaging.version import Version
 import os
+
+################# To refactor - I don't think these are used but they are mentioned in interview_generator.py
+class Object(object):
+  pass
+custom_values = Object()
+custom_values.people_plurals_map = {}
+custom_values.org_specific_config = None
+########################## End to refactor
 
 """
 Container for widely used data that may be altered by user inputs.
@@ -33,85 +40,12 @@ class SettingsList(DAList):
   def __str__(self):
     return "\n".join(self.complete_elements())
 
-
-################################# Old stuff to refactor
-class Object(object):
-  pass
-
-custom_values = Object()
-# This is to workaround fact you can't do local import in Docassemble playground
-
-# Var names of people-type lists that developers add while using the weaver.
-# Could be a list, but a dict will match RESERVED_PLURALIZERS_MAP in the
-# places it's used, so maybe easier to treat them both the same.
-custom_values.people_plurals_map = {}
-
-# Filled in by load_org_specific. Is a dictionary of configs that each org using
-# the Weaver can set, per installation. The specific configs are:
-# * jurisdiction_dependency_choices: a collection of other yaml files 
-#   that the generated interview will include, specifically a jurisdiction
-#   It's a map from keys, what the user will see when selecting dependencies, that maps to
-#   a list of 3 items: the pypi dependency, the yaml include, and a boolean for default selection
-custom_values.org_specific_config = None
-
-############################# End old stuff
-
-def get_possible_deps_as_choices(dep_category=None, all_custom=custom_values):
-  """Gets the possible yml files that the generated interview will depend on"""
-  
-  dep_choices = []
-  all_capabilities = load_capabilities()
-  for capability in all_capabilities:
-    if dep_category == 'organization':
-      dep_choices.extend([
-        {item.get('include_name'): item.get('description')}
-        for item in all_capabilities[capability].get('organization_choices',[])
-      ])
-    elif dep_category == 'jurisdiction':
-      dep_choices.extend([
-        {item.get('include_name'): item.get('description')}
-        for item in all_capabilities[capability].get('jurisdiction_choices',[])
-      ])
-
-  return dep_choices
-  
-  # load_org_specific(all_custom)
-  # dep_choices = all_custom.org_specific_config['dependency_choices']
-  # if dep_category is None:
-  #   return [{dep_key: dep_key, 'default': dep[3]} for dep_key, dep in dep_choices.items()]
-  # else:
-  #   return [{dep_key: dep_key, 'default': dep[3]} for dep_key, dep in dep_choices.items() 
-  #           if dep[2].lower() == dep_category.lower()]
-
-def get_pypi_deps_from_choices(choices:Union[List[str], DADict],
-    all_custom=custom_values):
-  """Gets the Pypi dependency requirement (i.e. docassemble.AssemblyLine>=2.0.19)
-  from some chosen dependencies"""
-  return get_values_from_choices(choices, 0)
-
-def get_yml_deps_from_choices(choices:Union[List[str], DADict],
-    all_custom_values=custom_values):
-  """Gets the yml file (i.e. docassemble.AssemblyLine:data/question/ql_baseline.yml)
-  from some chosen dependencies"""
-  return get_values_from_choices(choices, 1)
-
-def get_values_from_choices(choices:Union[List[str], DADict], value_idx:int=0,
-    all_custom_values=custom_values):
-  load_org_specific(all_custom_values)
-  if isinstance(choices, DADict):
-    choice_list = choices.true_values()
-  else: # List
-    choice_list = choices
-  
-  return [all_custom_values.org_specific_config['dependency_choices'][chosen_val][value_idx]
-      for chosen_val in choice_list]
-
-######################## pre load ###############################
-# This runs each time the .py file runs, which should be on each uwsgi reset
-
 def load_capabilities(base:str="docassemble.ALWeaver", minimum_version="1.5", include_playground=False):
-  # Get the contents of the current package's capabilities file.
-  # The local capabilities will always be the default configuration
+  """
+  Get the contents of the current package's capabilities file.
+  The local capabilities will always be the default configuration. Optionally, filter capabilities
+  by version and whether playground packages can provide capabilities.
+  """
   this_yaml = path_and_mimetype("data/sources/configuration_capabilities.yml")[0]
   weaverdata = DAStore(base=base)
   published_configuration_capabilities = weaverdata.get("published_configuration_capabilities") or {}
@@ -144,25 +78,51 @@ def load_capabilities(base:str="docassemble.ALWeaver", minimum_version="1.5", in
       except:
         log(f"Unable to load published Weaver configuration file {path}")
   
-  return capabilities
-      
-def _load_templates(self, template_path:str)->None:
-    """
-    Load YAML file with Mako templates into the templates attribute.
-    Overwrites any existing templates.
-    """
-    path = path_and_mimetype(template_path)[0]
-    with open(path) as f:
-      contents = f.read()
-    self.templates = list(yaml.safe_load_all(contents))[0] # Take the first YAML "document"    
-
-def advertise_capabilities(package_name:str=None, yaml_name:str="configuration_capabilities.yml", base:str="docassemble.ALWeaver", minimum_version="1.5"):
-  weaverdata = DAStore(base=base)
-  if not package_name:
-    package_name = __name__
-  published_configuration_capabilities = weaverdata.get("published_configuration_capabilities") or {}
-  published_configuration_capabilities[package_name] = (yaml_name, minimum_version)
-  weaverdata.set('published_configuration_capabilities', published_configuration_capabilities)
+  return capabilities  
   
-if not __name__ == '__main__' and not os.environ.get('ISUNITTEST'):
-  advertise_capabilities()
+_al_weaver_capabilities = load_capabilities()
+
+def get_possible_deps_as_choices(dep_category=None):
+  """Gets the possible yml files that the generated interview will depend on"""
+  
+  dep_choices = []  
+  
+  # TODO: do we want to prefix the choice with the package name?
+  for capability in _al_weaver_capabilities:
+    if dep_category == 'organization':
+      dep_choices.extend([
+        {item.get('include_name'): item.get('description')}
+        for item in _al_weaver_capabilities[capability].get('organization_choices',[])
+      ])
+    elif dep_category == 'jurisdiction':
+      dep_choices.extend([
+        {item.get('include_name'): item.get('description')}
+        for item in _al_weaver_capabilities[capability].get('jurisdiction_choices',[])
+      ])
+
+  return dep_choices
+
+def get_pypi_deps_from_choices(choices:Union[List[str], DADict]):
+  """Gets the Pypi dependency requirement (i.e. docassemble.AssemblyLine>=2.0.19)
+  from some chosen dependencies"""
+  pypi_deps = []
+  if isinstance(choices, DADict):
+    choice_list = choices.true_values()
+  else: # List
+    choice_list = choices
+    
+  for capability in _al_weaver_capabilities:
+    pypi_deps.extend([choice.get('dependency') for choice in _al_weaver_capabilities[capability].get('organization_choices',[]) + _al_weaver_capabilities[capability].get('jurisdiction_choices',[]) if choice.get('dependency') and choice.get('include_name') in choice_list])
+
+  return pypi_deps
+
+def get_yml_deps_from_choices(choices:Union[List[str], DADict]):
+  """Gets the yml file (i.e. docassemble.AssemblyLine:data/question/ql_baseline.yml)
+  from some chosen dependencies"""
+  # TODO
+  # We might want to prefix choices with the name of the package, and if so this would break
+  if isinstance(choices, DADict):
+    return choices.true_values()
+  else: # List
+    return choices
+    
