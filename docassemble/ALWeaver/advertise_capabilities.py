d281493c0c3eedbdead25f903c30c57c93e2469b
Quinten Steenhuis <qsteenhuis@gmail.com>
2022-01-10 09:54:48 -0500

    Use a DAStore to hold configuration settings (#536)
    * Start working on #528
    
    * Add new files for #528
    
    * prefix with __name__ instead of user_info().package_name
    
    * Testing a way to filter out the hook when running in a unittest
    
    * Sync to edit github workflow locally
    
    * Update unittest detection method
    
    * Move environment variable
    
    * Got the jurisdiction and org choices to load from the YAML file in #528
    
    * Get the config to work through download screen
    
    * Add the advertise_capabilities.py file
    
    * Clean up advertise_capabilities, add docstring and finalize unittest detection method
    
    * Remove version pin
    
    * Add boto3
    
    * Add another dependency
    
    * Add python-ldap build dependencies
    
    * Add dependency for libzbar0
    
    * Error handling for contexts where path_and_mimetype() will fail
    
    * Move __name__ shortening into advertise_capabilities so can be more flexible in implementing packages
    
    * Fix path for loading settings (use just the path, not tuple of path and mimetype)
    
    * Filter duplicates
    
    * Fix use of local version of config
    
    * Add more_itertools as dependency
    
    * Add a menu item to clear published configurations
    
    * Remove some other potentially future risky imports and relocate others to public namespaces

---
+++ docassemble/ALWeaver/advertise_capabilities.py
@@ -0,0 +1,10 @@
+# pre-load
+
+import os
+from .custom_values import advertise_capabilities
+
+__all__ = []
+  
+# If you want to prevent this script from running in unittests, add an environment variable ISUNITTEST set to TRUE  
+if not os.environ.get('ISUNITTEST'):
+  advertise_capabilities() 
--- docassemble/ALWeaver/custom_values.py
+++ docassemble/ALWeaver/custom_values.py
@@ -1,104 +1,171 @@
 from typing import List, Union
 from pathlib import Path
-import yaml
-from docassemble.base.util import log, DADict, objects_from_file
-# TODO(brycew): is this too deep into DA? Unclear if there are options to write
-# sources files to a package while it's running.
-from docassemble.base.functions import package_data_filename
+import ruamel.yaml as yaml
+from docassemble.base.util import log, DADict, DAList, DAStore, path_and_mimetype
+from packaging.version import Version
+from more_itertools import unique_everseen
 
-"""
-Container for widely used data that may be altered by user inputs.
-"""
-
-__all__ = ['get_possible_deps_as_choices', 'get_pypi_deps_from_choices', 
-           'get_yml_deps_from_choices']
-
-# This is to workaround fact you can't do local import in Docassemble playground
+################# To refactor - I don't think these are used but they are mentioned in interview_generator.py
 class Object(object):
   pass
-
 custom_values = Object()
-
-# Var names of people-type lists that developers add while using the weaver.
-# Could be a list, but a dict will match RESERVED_PLURALIZERS_MAP in the
-# places it's used, so maybe easier to treat them both the same.
 custom_values.people_plurals_map = {}
-
-# Filled in by load_org_specific. Is a dictionary of configs that each org using
-# the Weaver can set, per installation. The specific configs are:
-# * jurisdiction_dependency_choices: a collection of other yaml files 
-#   that the generated interview will include, specifically a jurisdiction
-#   It's a map from keys, what the user will see when selecting dependencies, that maps to
-#   a list of 3 items: the pypi dependency, the yaml include, and a boolean for default selection
 custom_values.org_specific_config = None
+########################## End to refactor
 
-def load_org_specific(all_custom_values=custom_values):
-  if all_custom_values.org_specific_config is None:
-    try:
-      all_custom_values.org_specific_config = objects_from_file('org_specific.yml')
-    except (FileNotFoundError, SystemError) as ex:
-      # Populate some default values
-      all_custom_values.org_specific_config = {
-        'dependency_choices': {
-          'Louisiana':
-            [
-              'docassemble.ALLouisianaSC',
-              'docassemble.ALLouisianaSC:custom_organization.yml',
-              'jurisdiction',
-              False
-            ],
-          'Illinois Legal Aid Online':
-            [
-              'docassemble.ILAO',
-              'docassemble.ILAO:ilao-interview-framework.yml',
-              'organization',
-              False
-            ],
-          'Massachusetts State': 
-              ['docassemble.ALMassachusetts>=0.0.7', 
-               'docassemble.ALMassachusetts:al_massachusetts.yml',
-               'jurisdiction',
-               True],
-          'MassAccess': 
-              ['docassemble.MassAccess',  
-               'docassemble.MassAccess:massaccess.yml',
-               'organization',
-               True]
-        }
-      }
-      to_write = package_data_filename('data/sources/org_specific.yml')
-      with open(to_write, 'w') as writ:
-        writ.write(yaml.safe_dump(all_custom_values.org_specific_config))
+__all__ = ['get_possible_deps_as_choices', 'get_pypi_deps_from_choices', 
+           'get_yml_deps_from_choices', 'SettingsList', 'load_capabilities',
+          'advertise_capabilities', '_package_name']
 
-def get_possible_deps_as_choices(dep_category=None, all_custom=custom_values):
+def _package_name(package_name:str=None):
+  """Get package name without the name of the current module, like: docassemble.ALWeaver instead of
+  docassemble.ALWeaver.advertise_capabilities"""
+  if not package_name:
+    package_name = __name__
+  try:
+    return ".".join(package_name.split(".")[:-1])
+  except:
+    return package_name
+
+class SettingsList(DAList):
+  """
+  A simple list that can sync itself to a DAStore. 
+  """
+  def init(self, *pargs, **kwargs):
+    super().init(*pargs, **kwargs)
+
+  def hook_after_gather(self):
+    if hasattr(self, 'store'):
+      self.store.set(self.instanceName, self)
+  
+  def __str__(self):
+    return "\n".join(self.complete_elements())
+
+def load_capabilities(base:str="docassemble.ALWeaver", minimum_version="1.5", include_playground=False):
+  """
+  Load and return a dictionary containing all advertised capabilities matching
+  the specified minimum version, and optionally include capabilities that were
+  advertised from a namespace matching docassemble.playground*. The local
+  capabilities will always be the default configuration.
+  """
+  current_package_name = _package_name()
+  
+  this_yaml = path_and_mimetype(f"{current_package_name}:data/sources/configuration_capabilities.yml")[0]
+  weaverdata = DAStore(base=base)
+  published_configuration_capabilities = weaverdata.get("published_configuration_capabilities") or {}
+  try:
+    with open(this_yaml) as f:
+      this_yaml_contents = f.read()
+
+    first_file = list(yaml.safe_load_all(this_yaml_contents))[0]
+
+    capabilities = {"Default configuration": first_file}    
+  except:
+    capabilities = {}
+  
+  for key in list(published_configuration_capabilities.keys()):
+    # Filter configurations based on minimum published version
+    if isinstance(published_configuration_capabilities[key], tuple) and Version(published_configuration_capabilities[key][1]) < Version(minimum_version):
+      log("Skipping published weaver configuration {key}:{published_configuration_capabilities[key]} because it is below the minimum version {minimum_version}. Consider updating the {key} package.")
+      del published_configuration_capabilities[key]
+    # Filter out capability files unless the package is installed system-wide
+    if not include_playground and key.startswith("docassemble.playground"):
+      del published_configuration_capabilities[key]
+    
+  for package_name in published_configuration_capabilities:
+    # Don't add the current package twice
+    if not current_package_name == package_name:
+      path = path_and_mimetype(f"{package_name}:data/sources/{published_configuration_capabilities[package_name][0]}")[0]
+      try:
+        with open(path) as f:
+          yaml_contents = f.read()
+        capabilities[package_name] = list(yaml.safe_load_all(yaml_contents))[0]
+      except:
+        log(f"Unable to load published Weaver configuration file {path}")
+  
+  return capabilities  
+  
+_al_weaver_capabilities = load_capabilities()
+
+def get_possible_deps_as_choices(dep_category=None):
   """Gets the possible yml files that the generated interview will depend on"""
-  load_org_specific(all_custom)
-  dep_choices = all_custom.org_specific_config['dependency_choices']
-  if dep_category is None:
-    return [{dep_key: dep_key, 'default': dep[3]} for dep_key, dep in dep_choices.items()]
-  else:
-    return [{dep_key: dep_key, 'default': dep[3]} for dep_key, dep in dep_choices.items() 
-            if dep[2].lower() == dep_category.lower()]
+  
+  dep_choices = []  
+  
+  # TODO: do we want to prefix the choice with the package name?
+  for capability in _al_weaver_capabilities:
+    if dep_category == 'organization':
+      dep_choices.extend([
+        {item.get('include_name'): item.get('description')}
+        for item in _al_weaver_capabilities[capability].get('organization_choices',[])
+      ])
+    elif dep_category == 'jurisdiction':
+      dep_choices.extend([
+        {item.get('include_name'): item.get('description')}
+        for item in _al_weaver_capabilities[capability].get('jurisdiction_choices',[])
+      ])
 
-def get_pypi_deps_from_choices(choices:Union[List[str], DADict],
-    all_custom=custom_values):
+  return list(unique_everseen(dep_choices))
+
+def get_pypi_deps_from_choices(choices:Union[List[str], DADict]):
   """Gets the Pypi dependency requirement (i.e. docassemble.AssemblyLine>=2.0.19)
   from some chosen dependencies"""
-  return get_values_from_choices(choices, 0)
-
-def get_yml_deps_from_choices(choices:Union[List[str], DADict],
-    all_custom_values=custom_values):
-  """Gets the yml file (i.e. docassemble.AssemblyLine:data/question/ql_baseline.yml)
-  from some chosen dependencies"""
-  return get_values_from_choices(choices, 1)
-
-def get_values_from_choices(choices:Union[List[str], DADict], value_idx:int=0,
-    all_custom_values=custom_values):
-  load_org_specific(all_custom_values)
+  pypi_deps = []
   if isinstance(choices, DADict):
     choice_list = choices.true_values()
   else: # List
     choice_list = choices
-  
-  return [all_custom_values.org_specific_config['dependency_choices'][chosen_val][value_idx] 
-      for chosen_val in choice_list] 
+    
+  for capability in _al_weaver_capabilities:
+    pypi_deps.extend([choice.get('dependency') for choice in _al_weaver_capabilities[capability].get('organization_choices',[]) + _al_weaver_capabilities[capability].get('jurisdiction_choices',[]) if choice.get('dependency') and choice.get('include_name') in choice_list])
+
+  return list(unique_everseen(pypi_deps))
+
+def get_yml_deps_from_choices(choices:Union[List[str], DADict]):
+  """Gets the yml file (i.e. docassemble.AssemblyLine:data/question/ql_baseline.yml)
+  from some chosen dependencies"""
+  # TODO
+  # We might want to prefix choices with the name of the package, and if so this would break
+  if isinstance(choices, DADict):
+    return choices.true_values()
+  else: # List
+    return choices
+    
+def advertise_capabilities(package_name:str=None, yaml_name:str="configuration_capabilities.yml", base:str="docassemble.ALWeaver", minimum_version="1.5"):
+  """
+  Tell the server that the current Docassemble package contains a
+  configuration_capabilities.yml file with settings that ALWeaver can use, by
+  adding an entry to the global DAStore. 
+
+  Defaults to work with standard Docassemble package names. If the package_name
+  has 3 parts or more, the last part will be dropped. E.g., it will advertise
+  "docassemble.ALWeaver", not "docassemble.ALWeaver.custom_values".
+
+  This function should be imported and run inside any Docassemble package that
+  contains settings that you want to advertise to the installed Weaver on a
+  given server. This function should be set to run with a # pre-load hook so it
+  advertises itself on each server uwsgi reset.
+
+  Example of an `advertise_capabilities.py` file you can include in your own package:
+  ```python
+  # pre-load
+
+  import os
+  from docassemble.ALWeaver.custom_values import advertise_capabilities
+
+  if not os.environ.get('ISUNITTEST'):
+    advertise_capabilities(__name__, minimum_version="1.5")
+  ```
+  """
+  weaverdata = DAStore(base=base)
+  if not package_name:
+    package_name = _package_name()
+  elif isinstance(package_name, str):
+    package_name_parts = package_name.split(".")
+    if len(package_name_parts) > 2:
+      package_name = ".".join(package_name_parts[:-1])
+  published_configuration_capabilities = weaverdata.get("published_configuration_capabilities") or {}
+  if not isinstance(published_configuration_capabilities, dict):
+    published_configuration_capabilities = {}
+  published_configuration_capabilities[package_name] = [yaml_name, minimum_version]
+  weaverdata.set('published_configuration_capabilities', published_configuration_capabilities)
