889df1725183f44a169fb89abdb096106d6d0f62
Quinten Steenhuis <qsteenhuis@gmail.com>
2022-01-13 11:07:17 -0500

    Reformatted with black

--- docassemble/ALWeaver/advertise_capabilities.py
+++ docassemble/ALWeaver/advertise_capabilities.py
@@ -4,7 +4,7 @@ import os
 from .custom_values import advertise_capabilities
 
 __all__ = []
-  
-# If you want to prevent this script from running in unittests, add an environment variable ISUNITTEST set to TRUE  
-if not os.environ.get('ISUNITTEST'):
-  advertise_capabilities() 
+
+# If you want to prevent this script from running in unittests, add an environment variable ISUNITTEST set to TRUE
+if not os.environ.get("ISUNITTEST"):
+    advertise_capabilities()
