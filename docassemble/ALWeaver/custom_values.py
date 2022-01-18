e04caa2050899b3a5141afbb208ba7e2bd5caaf2
Quinten Steenhuis <qsteenhuis@gmail.com>
2022-01-18 13:38:40 -0500

    Fix #548 - I'm feeling lucky button now works with the new custom config code

--- docassemble/ALWeaver/custom_values.py
+++ docassemble/ALWeaver/custom_values.py
@@ -23,6 +23,8 @@ __all__ = [
     "load_capabilities",
     "advertise_capabilities",
     "_package_name",
+    "get_full_dep_details",
+    "get_matching_deps"
 ]
 
 
@@ -177,7 +179,61 @@ def get_yml_deps_from_choices(choices: Union[List[str], DADict]):
     else:  # List
         return choices
 
+def get_full_dep_details(dep_category=None):
+    """Get the full dictionary describing each dependency (including custom ones) so that it can be
+    filtered and used as needed."""
+    dep_choices = []
 
+    # TODO: do we want to prefix the choice with the package name?
+    for capability in _al_weaver_capabilities:
+        if dep_category == "organization":
+            dep_choices.extend(
+                _al_weaver_capabilities[capability].get(
+                        "organization_choices", []
+                    )
+            )
+        elif dep_category == "jurisdiction":
+            dep_choices.extend(_al_weaver_capabilities[capability].get(
+                        "jurisdiction_choices", []
+                    )
+            )
+
+    return list(unique_everseen(dep_choices))
+
+def get_matching_deps(dep_category=None, state=None):
+    """Get the dependencies that match the specified state. 
+    State "ANY" has a special meaning."""
+    dep_choices = []
+
+    # TODO: do we want to prefix the choice with the package name?
+    for capability in _al_weaver_capabilities:
+        if dep_category == "organization":
+            dep_choices.extend(
+                [
+                    item.get("include_name")
+                    for item in _al_weaver_capabilities[capability].get(
+                        "organization_choices", []
+                    ) if item.get("state","").lower() == state.lower()
+                ]
+            )
+        elif dep_category == "jurisdiction":
+            dep_choices.extend(
+                [
+                    item.get("include_name")
+                    for item in _al_weaver_capabilities[capability].get(
+                        "jurisdiction_choices", []
+                    ) if item.get("state","").lower() == state.lower()
+                ]
+            )
+    
+    if len(dep_choices) > 0 or state.lower() == "any":
+      return DADict(elements={item:True for item in unique_everseen(dep_choices)},
+                    auto_gather=False, gathered=True
+                   )
+    
+    return get_matching_deps(dep_category=dep_category, state="ANY")
+    
+  
 def advertise_capabilities(
     package_name: str = None,
     yaml_name: str = "configuration_capabilities.yml",
