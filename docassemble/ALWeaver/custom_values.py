8a4f9dc06d6ebe0d5105ba18091f4ea25834fa6b
BryceStevenWilley <BryceStevenWilley@users.noreply.github.com>
2022-01-19 22:33:40 +0000

    :art: Format Python code with psf/black

--- docassemble/ALWeaver/custom_values.py
+++ docassemble/ALWeaver/custom_values.py
@@ -180,7 +180,7 @@ def get_yml_deps_from_choices(choices: Union[List[str], DADict]):
         return choices
 
 
-def get_full_dep_details(dep_category:str=None) -> List:
+def get_full_dep_details(dep_category: str = None) -> List:
     """Get the full dictionary describing each dependency (including custom ones) so that it can be
     filtered and used as needed."""
     dep_choices = []
@@ -199,7 +199,7 @@ def get_full_dep_details(dep_category:str=None) -> List:
     return list(unique_everseen(dep_choices))
 
 
-def get_matching_deps(dep_category:str=None, state:str=None) -> DADict:
+def get_matching_deps(dep_category: str = None, state: str = None) -> DADict:
     """Get the dependencies that match the specified state.
     State "ANY" has a special meaning."""
     dep_choices = []
