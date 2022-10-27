6ad3699b7624ce4bf55422648bd369d3f7830fe2
BryceStevenWilley <BryceStevenWilley@users.noreply.github.com>
2022-10-27 15:41:12 +0000

    :art: Format Python code with psf/black

--- docassemble/ALWeaver/test_pdf_files.py
+++ docassemble/ALWeaver/test_pdf_files.py
@@ -1,12 +1,13 @@
 import unittest
 from .interview_generator import (
-  DAFieldList,
-  get_variable_name_warnings,
-  get_pdf_validation_errors
+    DAFieldList,
+    get_variable_name_warnings,
+    get_pdf_validation_errors,
 )
 from docassemble.base.util import DAStaticFile
 import docassemble.base.functions
 from pathlib import Path
+
 
 class MockDAStaticFile(DAStaticFile):
     def init(self, *pargs, **kwargs):
@@ -26,18 +27,23 @@ class MockDAStaticFile(DAStaticFile):
     def path(self):
         return self.full_path
 
+
 class test_pdfs(unittest.TestCase):
     def test_validate_ignore_push_button(self):
         push_button_pdf = Path(__file__).parent / "test/test_push_button.pdf"
         docassemble.base.functions.this_thread.current_question = type("", (), {})
         docassemble.base.functions.this_thread.current_question.package = "ALWeaver"
-        da_pdf = MockDAStaticFile(full_path=str(push_button_pdf), extension='pdf', mimetype='application/pdf')
+        da_pdf = MockDAStaticFile(
+            full_path=str(push_button_pdf), extension="pdf", mimetype="application/pdf"
+        )
         fields = DAFieldList()
         fields.add_fields_from_file(da_pdf)
         fields.gathered = True
         bad_fields = get_variable_name_warnings(fields)
-        self.assertEqual(len(bad_fields), 0, f"Bad fields in test_push_button.pdf: {bad_fields}")
+        self.assertEqual(
+            len(bad_fields), 0, f"Bad fields in test_push_button.pdf: {bad_fields}"
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
-
